a28e5704786684ad48fbbf37385d24af1b5557c8
Robert Canton <robshanecanton@gmail.com>
2017-09-18 02:28:32 -0400

    Minor change

--- ExpandingCellTransition/Extensions.swift
+++ ExpandingCellTransition/Extensions.swift
@@ -10,26 +10,15 @@ import UIKit
 
 extension UIView {
     
-    /// Create snapshot
-    ///
-    /// - parameter rect: The `CGRect` of the portion of the view to return. If `nil` (or omitted),
-    ///                   return snapshot of the whole view.
-    ///
-    /// - returns: Returns `UIImage` of the specified portion of the view.
-    
     func snapshot(of rect: CGRect? = nil) -> UIImageView? {
-        // snapshot entire view
         
         UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
         drawHierarchy(in: bounds, afterScreenUpdates: false)
         let wholeImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        // if no `rect` provided, return image of whole view
         
         guard let image = wholeImage, let rect = rect else { return nil }
-        
-        // otherwise, grab specified `rect` of image
         
         let scale = image.scale
         let scaledRect = CGRect(x: rect.origin.x * scale, y: rect.origin.y * scale, width: rect.size.width * scale, height: rect.size.height * scale)
